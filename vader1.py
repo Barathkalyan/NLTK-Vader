750f2b49c09c38f5cda3048a07af60fff7f3c845
Barathkalyan <145141278+Barathkalyan@users.noreply.github.com>
2025-02-23 21:01:19 +0530

    Update vader1.py

--- vader1.py
+++ vader1.py
@@ -1,5 +1,10 @@
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 sia=SentimentIntensityAnalayzer()
+sentences = ["I love this!", "I hate this!", "Oh great, more work...", "I feel okay today."]
+for text in sentences:
+    print(f"Sentence: {text}")
+    print(sia.polarity_scores(text))
+
 
 
